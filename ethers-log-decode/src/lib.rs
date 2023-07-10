282fe5304641bbb0366da07f64ef6b557e242578
Fedor Sakharov <fedor.sakharov@gmail.com>
2023-07-10 14:56:30 +0300

    different types and add doc test to ci

--- ethers-log-decode/src/lib.rs
+++ ethers-log-decode/src/lib.rs
@@ -9,7 +9,7 @@ use syn::{parse_macro_input, spanned::Spanned, Data, DeriveInput, Error, Fields}
 /// # Examples:
 ///
 /// ```
-/// use ethers::prelude::EthLogDecode;
+/// use ethers::prelude::{abigen, EthLogDecode};
 /// use ethers_log_decode::EthLogDecode;
 ///
 /// abigen!(
@@ -29,9 +29,40 @@ use syn::{parse_macro_input, spanned::Spanned, Data, DeriveInput, Error, Fields}
 ///     
 /// #[derive(EthLogDecode)]
 /// enum Events {
-///     Approval(IERC20Events::ApprovalFilter),
-///     Transfer(IERC20Events::TransferFilter),
-///     BlockCommit(IExecutor::BlockCommitFilter),
+///     Approval(ApprovalFilter),
+///     Transfer(TransferFilter),
+///     BlockCommit(BlockCommitFilter),
+/// }
+/// ```
+///
+/// The variant types should all be different:
+///
+/// ```compile_fail
+///
+/// use ethers::prelude::{abigen, EthLogDecode};
+/// use ethers_log_decode::EthLogDecode;
+///
+/// abigen!(
+///    IERC20,
+///    r#"[
+///        event Transfer(address indexed from, address indexed to, uint256 value)
+///        event Approval(address indexed owner, address indexed spender, uint256 value)
+///    ]"#,
+/// );
+///
+/// abigen!(
+///     IExecutor,
+///     r#"[
+///         event BlockCommit(uint256 indexed blockNumber, bytes32 indexed blockHash, bytes32 indexed commitment)
+///     ]"#,
+/// );
+///     
+/// #[derive(EthLogDecode)]
+/// enum Events {
+///     Approval(ApprovalFilter),
+///     Transfer(TransferFilter),
+///     BlockCommit(BlockCommitFilter),
+///     BlockCommit2(BlockCommitFilter),
 /// }
 /// ```
 ///
@@ -81,10 +112,20 @@ pub fn my_eth_log_decode_derive(input: TokenStream) -> TokenStream {
                     }
                 };
 
+                if types.contains(&t) {
+                    return Error::new(
+                        input.span(),
+                        "EthLogDecode enum should contain variants of different types".to_string(),
+                    )
+                    .to_compile_error()
+                    .into();
+                }
+
                 types.push(t);
                 ident.push(variant.ident);
             }
 
+            let types: Vec<_> = types.into_iter().collect();
             quote! {
                 impl ethers::prelude::EthLogDecode for #name {
                     fn decode_log(log: &ethers::abi::RawLog) -> core::result::Result<Self, ethers::abi::Error> {
