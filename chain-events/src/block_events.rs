d032dc72cd9803a91c60df095cc6715eba6e5fc4
Fedor Sakharov <fedor.sakharov@gmail.com>
2023-07-12 16:14:46 +0300

    equips panic messages with info about failed object (#30)
    * equips panic messages with info about failed object
    
    * actually format things
    
    * fix clippy

--- chain-events/src/block_events.rs
+++ chain-events/src/block_events.rs
@@ -234,12 +234,14 @@ async fn process_l1_event<M, S>(
             let Ok(tx) = middleware
                 .get_transaction(
                     log.transaction_hash
-                        .expect("log always has a related transaction; qed"),
+                        .unwrap_or_else(|| panic!("log always has a related transaction {:?}; qed", log)),
                 )
                 .await
                 else { return };
 
-            let tx = tx.expect("mined transaction exists; qed");
+            let tx = tx.unwrap_or_else(|| {
+                panic!("mined transaction exists {:?}; qed", log.transaction_hash)
+            });
 
             let mut events = vec![];
 
@@ -247,7 +249,9 @@ async fn process_l1_event<M, S>(
                 let mut res = parse_withdrawal_events_l1(
                     &commit_blocks,
                     tx.block_number
-                        .expect("a mined transaction has a block number; qed")
+                        .unwrap_or_else(|| {
+                            panic!("a mined transaction {:?} has a block number; qed", tx.hash)
+                        })
                         .as_u64(),
                     l2_erc20_bridge_addr,
                 );
