c7b69c8f8a49c0cad8c29f54acfe125c68928921
Fedor Sakharov <fedor.sakharov@gmail.com>
2023-07-18 15:28:31 +0300

    adds finalization_data table migrations

---
+++ storage/migrations/20230717075048_finalizer.up.sql
@@ -0,0 +1,19 @@
+CREATE TABLE finalization_data (
+    tx_hash BYTEA NOT NULL,
+    event_index_in_tx INT NOT NULL,
+
+    l2_block_number BIGINT NOT NULL,
+    l1_batch_number BIGINT NOT NULL,
+    l2_message_index INT NOT NULL,
+    l2_tx_number_in_block SMALLINT NOT NULL,
+    message BYTEA NOT NULL,
+    sender BYTEA NOT NULL,
+    proof BYTEA NOT NULL,
+
+    finalization_tx BYTEA DEFAULT NULL,
+    failed_finalization_attempts BIGINT DEFAULT 0,
+
+    PRIMARY KEY (tx_hash, event_index_in_tx),
+
+    FOREIGN KEY (tx_hash, event_index_in_tx) REFERENCES withdrawals (tx_hash, event_index_in_tx)
+);
