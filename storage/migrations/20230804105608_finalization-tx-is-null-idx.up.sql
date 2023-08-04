9020c542a3e049df239ff754e7b02ca973b00556
Fedor Sakharov <fedor.sakharov@gmail.com>
2023-08-04 18:48:15 +0300

    name the index

--- storage/migrations/20230804105608_finalization-tx-is-null-idx.up.sql
+++ storage/migrations/20230804105608_finalization-tx-is-null-idx.up.sql
@@ -1,2 +1,2 @@
 DROP INDEX fd_l2_block_number_ix;
-CREATE INDEX ON finalization_data (l2_block_number, finalization_tx) WHERE finalization_tx IS NULL;
+CREATE INDEX fd_l2_block_number_finalization_tx_ix ON finalization_data (l2_block_number, finalization_tx) WHERE finalization_tx IS NULL;
