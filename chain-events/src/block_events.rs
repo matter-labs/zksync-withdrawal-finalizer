94c1e8194023ad7ff546ce73f4269589182386b2
Fedor Sakharov <fedor.sakharov@gmail.com>
2023-10-02 12:49:52 +0200

    fix: adds logging to block execution events

--- chain-events/src/block_events.rs
+++ chain-events/src/block_events.rs
@@ -275,8 +275,10 @@ where
             )
             .await
             else {
-                return Ok(());
+                vlog::error!("Failed to retreive transaction {:?}", log.transaction_hash);
+                return Err(Error::NoTransaction);
             };
+
             let tx = tx.unwrap_or_else(|| {
                 panic!("mined transaction exists {:?}; qed", log.transaction_hash)
             });
@@ -320,6 +322,8 @@ where
                 .map_err(|_| Error::ChannelClosing)?;
         }
         L1Events::BlocksExecution(event) => {
+            vlog::info!("Received a block execution event {event:?} {tx:?}");
+
             metrics::increment_counter!("watcher.chain_events.block_execution_events");
             sender
                 .send(BlockEvent::BlockExecution {
