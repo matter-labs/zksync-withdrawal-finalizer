62abf7b03a1bb7aa228238f5a13cbdb16a3fbf59
Danil <deniallugo@gmail.com>
2025-08-13 14:21:55 +0200

    Fix storage
    Signed-off-by: Danil <deniallugo@gmail.com>

--- storage/migrations/20250813112108_add-indexes-for-l2-blocks.down.sql
+++ storage/migrations/20250813112108_add-indexes-for-l2-blocks.down.sql
@@ -1,1 +1,1 @@
-DROP INDEX IF EXISTS l2_blocks_number_chain_id;
+DROP INDEX IF EXISTS l2_blocks_chain_id;
--- storage/migrations/20250813112108_add-indexes-for-l2-blocks.up.sql
+++ storage/migrations/20250813112108_add-indexes-for-l2-blocks.up.sql
@@ -1,1 +1,1 @@
-CREATE INDEX IF NOT EXISTS l2_blocks_number_chain_id ON l2_blocks(commit_chain_id, l2_block_number);
+CREATE INDEX IF NOT EXISTS l2_blocks_chain_id ON l2_blocks(commit_chain_id);
