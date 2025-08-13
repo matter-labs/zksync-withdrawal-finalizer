aad6a1eafa780eba3f0e628b092d5106668b5f35
Danil <deniallugo@gmail.com>
2025-08-13 17:43:49 +0200

    feat(db): get the the first block to process properly (#546)
    # What ❔
    
    <!-- What are the changes this PR brings about? -->
    <!-- Example: This PR adds a PR template to the repo. -->
    <!-- (For bigger PRs adding more context is appreciated) -->
    
    ## Why ❔
    
    <!-- Why are these changes done? What goal do they contribute to? What
    are the principles behind them? -->
    <!-- Example: PR templates ensure PR reviewers, observers, and future
    iterators are in context about the evolution of repos. -->
    
    ## Checklist
    
    <!-- Check your PR fulfills the following items. -->
    <!-- For draft PRs check the boxes as you complete them. -->
    
    - [ ] PR title corresponds to the body of PR (we generate changelog
    entries from PRs).
    - [ ] Tests for the changes have been added / updated.
    - [ ] Documentation comments have been added / updated.
    - [ ] Code has been formatted via `cargo fmt`.
    
    ---------
    
    Signed-off-by: Danil <deniallugo@gmail.com>

---
+++ storage/migrations/20250813112108_add-indexes-for-l2-blocks.down.sql
@@ -0,0 +1,1 @@
+DROP INDEX IF EXISTS l2_blocks_chain_id;
--- storage/src/lib.rs
+++ storage/src/lib.rs
@@ -366,6 +366,56 @@ pub async fn last_l1_block_seen(
     Ok(res)
 }
 
+/// Get the last block that has been processed on the previous SL.
+pub async fn last_processed_l2_block_on_the_previous_chain(
+    conn: &mut PgConnection,
+    chain_id: u32,
+) -> Result<u64> {
+    let latency = STORAGE_METRICS.call[&"last_processed_l2_block"].start();
+    let res = sqlx::query!(
+        "
+          SELECT l2_block_number
+          FROM l2_blocks
+          WHERE commit_chain_id != $1
+          ORDER BY l2_block_number DESC
+          LIMIT 1
+        ",
+        chain_id as i32
+    )
+    .fetch_optional(conn)
+    .await?
+    .map(|v| v.l2_block_number as u64)
+    .unwrap_or(0);
+
+    latency.observe();
+
+    Ok(res)
+}
+
+/// Get the last block that has been processed on the null chain.
+/// It's a transition hack, otherwise lookup for the whole table of L2 blocks takes an eternity
+pub async fn last_processed_l2_block_with_null(conn: &mut PgConnection) -> Result<Option<u64>> {
+    let latency = STORAGE_METRICS.call[&"last_processed_l2_block"].start();
+    let res = sqlx::query!(
+        "
+        SELECT
+          max(l2_block_number)
+        FROM
+          l2_blocks
+        WHERE
+          commit_chain_id is NULL
+        ",
+    )
+    .fetch_one(conn)
+    .await?
+    .max
+    .map(|max| max as u64);
+
+    latency.observe();
+
+    Ok(res)
+}
+
 /// Adds a `L2ToL1Event` set to the DB.
 ///
 /// # Arguments
